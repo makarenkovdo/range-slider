094f9d397653335328d85d54a4b7d95f6bea57b5
makarenkovdo <makarenkovdo@gmail.com>
2021-10-15 12:21:59 +0300

    create and building at once

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import jquery from 'jquery'
 import SliderController from './controller/slider-controller'
 
-//создавать нужно тут new Slider('#айдишник')
 const firstSlider = new SliderController('first')
-const secondSlider = new SliderController('second')
-const thirdSlider = new SliderController('third')
-firstSlider
     .switchOnTip()
     .setMinValue(5)
     .setMaxValue(77)
     .init()
     .onDrag()
     .onDrop()
-secondSlider.switchOnTip().setMaxValue(333).init().onDrag().onDrop()
-thirdSlider.addSliderButton().setMaxValue(123).init().onDrag().onDrop()
+const secondSlider = new SliderController('second')
+    .switchOnTip()
+    .setMaxValue(333)
+    .init()
+    .onDrag()
+    .onDrop()
+const thirdSlider = new SliderController('third')
+    .addSliderButton()
+    .setMaxValue(123)
+    .init()
+    .onDrag()
+    .onDrop()
 
 /*for configurate initial parameters just send attributes to builder and
 and give user possibility to change it 

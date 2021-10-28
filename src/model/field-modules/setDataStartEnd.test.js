bc62d172a429aacc5ae2bb3e9677d198be99e027
makarenkovdo <makarenkovdo@gmail.com>
2021-10-28 13:19:11 +0300

    cleaning

--- src/model/field-modules/setDataStartEnd.test.js
+++ src/model/field-modules/setDataStartEnd.test.js
@@ -3,14 +3,7 @@
  */
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/dom';
-import jQuery from 'jquery';
-// if you want to use $ as an alias
-const $ = jQuery;
 import SliderController from '../../controller/slider-controller';
-
-// const jsdom = require('jsdom');
-// const { JSDOM } = jsdom;
-// const { window } = new JSDOM(`<!DOCTYPE html>`);
 
 beforeEach(() => {
   document.body.innerHTML = `

7b0f5520b57578052b6b6504e715e0c02b6882dc
makarenkovdo <makarenkovdo@gmail.com>
2021-10-29 06:42:24 +0300

    fix bar bug

--- src/view/viewModules/createBar.ts
+++ src/view/viewModules/createBar.ts
@@ -3,10 +3,10 @@ import SliderView from '../SliderView';
 // how to test? this.$bar is a the result of div-appending and ALL THIS MODULE
 const createBar = function addBarToDomAndSetThisBar(this: SliderView): void {
   const addBarToDom = () => {
-    this.$field.append("<div class='runner-bar'></div>");
+    this.$field.append("<div class='slider-bar'></div>");
   };
   const setThis$bar = () => {
-    this.$bar = this.$field.children('.runner-bar');
+    this.$bar = this.$field.children('.slider-bar');
   };
   addBarToDom();
   setThis$bar();

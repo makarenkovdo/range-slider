cfa729fc993c529a90dbb6b6d1aa36fc937f1082
makarenkovdo <makarenkovdo@gmail.com>
2021-10-31 08:34:46 +0300

    test defineRunnerType

---
+++ test/pureFunctions/SliderView/defineRunnerType.test.ts
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom';
+import { defineRunnerType } from '../../../src/view/viewModules/updateRunnerPosition/updateRunnerPositionUtility';
+
+beforeEach(() => {
+  document.body.innerHTML = `
+      <div data-testid="testId" id="testId" class="range-runner horizontal" data-start="0"></div>
+      `;
+});
+
+describe('if function "defineRunnerType" return object properties', () => {
+  test('must return 70', () => {
+    expect(defineRunnerType(false, 90, 0)).toHaveProperty('instance', 0);
+    expect(defineRunnerType(false, 90, 0)).toHaveProperty('stepPosition', 90);
+  });
+});

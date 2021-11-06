4a0f9c4190ce52d60bfef68e6a1c6163f057a4df
makarenkovdo <makarenkovdo@gmail.com>
2021-11-06 09:58:19 +0300

    bem-bar

--- src/view/viewModules/createBar.ts
+++ src/view/viewModules/createBar.ts
@@ -3,12 +3,13 @@ import SliderView from '../SliderView';
 // how to test? this.$bar is a the result of div-appending and ALL THIS MODULE
 const createBar = function addBarToDomAndSetThisBar(this: SliderView): void {
   const addBarToDom = () => {
+    const modificator = this.isVertical ? 'vertical' : 'horizontal';
     this.$field.append(
-      "<div data-testid='test-slider-bar' class='slider-bar js-slider-bar'></div>",
+      `<div data-testid='test-slider-bar' class='slider__bar slider__bar_${modificator} js-slider__bar'></div>`,
     );
   };
   const setThis$bar = () => {
-    this.$bar = this.$field.children('.js-slider-bar');
+    this.$bar = this.$field.children('.js-slider__bar');
   };
   addBarToDom();
   setThis$bar();

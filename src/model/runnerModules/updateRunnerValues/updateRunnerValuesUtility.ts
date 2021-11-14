283c27c1c4ea5783177a19ed913a2fc829d0cf01
makarenkovdo <makarenkovdo@gmail.com>
2021-11-14 17:35:49 +0300

    last runner value (if exception case)

--- src/model/runnerModules/updateRunnerValues/updateRunnerValuesUtility.ts
+++ src/model/runnerModules/updateRunnerValues/updateRunnerValuesUtility.ts
@@ -105,17 +105,27 @@ fieldSize: number[],
 isVertical: boolean):CheckCollisionSubargs => {
   const positioningSwitcher = isVertical ? 1 : 0;
   let divisionQuantity = 0;
-  if (Math.floor((minMax[1] - minMax[0]) / step) * step === (minMax[1] - minMax[0])) {
-    divisionQuantity = (minMax[1] - minMax[0]) / step;
-  } else divisionQuantity = Math.floor((minMax[1] - minMax[0]) / step) + 1;
+  function checkOnException () {
+    return Math.floor((minMax[1] - minMax[0]) / step) * step === (minMax[1] - minMax[0])
+  }
+  if (checkOnException()) divisionQuantity = (minMax[1] - minMax[0]) / step;
+  else divisionQuantity = Math.floor((minMax[1] - minMax[0]) / step) + 1;
   console.log('value', value);
   const divisionSizeInPercent = 100 / divisionQuantity;
 
-  const stepPosition = Number((Math.round(positionInPercent / stepInPercent) * (stepInPercent))
+  let stepPosition = Number((Math.round(positionInPercent / stepInPercent) * (stepInPercent))
     .toFixed(stepSignAfterComma));
   const stepValueMultiplier = Math.floor(stepPosition / divisionSizeInPercent);
-  const stepValue = Number((minMax[0] + step * stepValueMultiplier));
-  console.log('stepValueMultiplier', stepValueMultiplier);
+  let stepValue = Number((minMax[0] + step * stepValueMultiplier));
+  console.log(checkOnException());
+  
+  if (!checkOnException() && stepValueMultiplier === divisionQuantity) {
+    console.log('???');
+    
+    stepPosition = 100;
+    stepValue = minMax[1];
+  }
+  console.log('stepValue', positionInPercent);
 
   // const stepValue = Number((Math.round(value / step) * step).toFixed(stepSignAfterComma));
   return { stepPosition, stepValue };

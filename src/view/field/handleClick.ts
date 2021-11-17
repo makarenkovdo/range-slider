cd246bb1df70e2660a9f37246bac45bb12b20806
makarenkovdo <makarenkovdo@gmail.com>
2021-11-17 12:01:31 +0300

    onClick ON +

--- src/view/field/handleClick.ts
+++ src/view/field/handleClick.ts
@@ -2,9 +2,9 @@ import SliderView from '../SliderView';
 import handleClickUtility from './handleClick/handleClickUtility';
 
 const handleClick = function activateOnClickListenerAndNotify(this: SliderView): void {
-  // this.$field.on('click', this, handleClickUtility);
-  // $('.js-scale-line').on('click', this, handleClickUtility);
-  // $('.js-scale-numbers').on('click', this, handleClickUtility);
+  this.$field.on('click', this, handleClickUtility);
+  $('.js-scale-line').on('click', this, handleClickUtility);
+  $('.js-scale-numbers').on('click', this, handleClickUtility);
 };
 
 export default handleClick;

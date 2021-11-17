5cb0abb458e3232d83caf7482c4c09c7ae2fddb1
makarenkovdo <makarenkovdo@gmail.com>
2021-11-17 19:23:42 +0300

    refactor setSignAfterComma to solve a typeError

--- src/model/runnerModules/setSignAfterComma/setSignAfterCommaUtility.ts
+++ src/model/runnerModules/setSignAfterComma/setSignAfterCommaUtility.ts
@@ -2,23 +2,17 @@ import RunnerModel from '../../RunnerModel';
 
 const calcSignAfterComma = ({ step }: RunnerModel, minMax: number[]): number => {
   const minMaxSignAfterComma:number[] = [];
-  if (step.toString().includes('.')) {
-    minMaxSignAfterComma.push(step
-      .toString()
-      .split('.')
-      .pop().length);
+  const separatedStep = step.toString().split('.');
+  const separatedMin = minMax[0].toString().split('.');
+  const separatedMax = minMax[1].toString().split('.');
+  if (separatedStep[1]) {
+    minMaxSignAfterComma.push(separatedStep.pop().length);
   } else minMaxSignAfterComma.push(0);
-  if (minMax[0].toString().includes('.')) {
-    minMaxSignAfterComma.push(minMax[0]
-      .toString()
-      .split('.')
-      .pop().length);
+  if (separatedMin[1]) {
+    minMaxSignAfterComma.push(separatedMin.pop().length);
   } else minMaxSignAfterComma.push(0);
-  if (minMax[1].toString().includes('.')) {
-    minMaxSignAfterComma.push(minMax[1]
-      .toString()
-      .split('.')
-      .pop().length);
+  if (separatedMax[1]) {
+    minMaxSignAfterComma.push(separatedMax.pop().length);
   } else minMaxSignAfterComma.push(0);
 
   return Math.max(minMaxSignAfterComma[0], minMaxSignAfterComma[1], minMaxSignAfterComma[2]);

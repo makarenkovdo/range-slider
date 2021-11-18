cc7892fae39d9e6fa67c23fb177409c4dec31aee
makarenkovdo <makarenkovdo@gmail.com>
2021-11-18 11:27:58 +0300

    fix calculateStepValueAndPosition test

--- test/Model/Runner/pureFunctions/calculateStepValueAndPosition.test.ts
+++ test/Model/Runner/pureFunctions/calculateStepValueAndPosition.test.ts
@@ -16,18 +16,19 @@ describe('calculateValue test', () => {
     const CalculateStepValueAndPositionArgsTest: CalculateStepValueAndPositionArgs = {
       positionInPercent: 23,
       step: 2,
+      stepInPercent: 20,
       value: 23,
       stepSignAfterComma: 0,
     };
 
-    expect(calculateStepValueAndPosition(CalculateStepValueAndPositionArgsTest)).toHaveProperty(
+    expect(calculateStepValueAndPosition(CalculateStepValueAndPositionArgsTest, [0, 10])).toHaveProperty(
       'stepPosition',
-      24,
+      20,
     );
 
-    expect(calculateStepValueAndPosition(CalculateStepValueAndPositionArgsTest)).toHaveProperty(
+    expect(calculateStepValueAndPosition(CalculateStepValueAndPositionArgsTest, [0, 10])).toHaveProperty(
       'stepValue',
-      24,
+      2,
     );
   });
 });
